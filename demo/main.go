0c524113ad490664b35f25f2867542412b8fda40
elsonwu <elsonwu@126.com>
2013-08-17 13:20:27 +0800

    improve the error report

--- demo/main.go
+++ demo/main.go
@@ -3,43 +3,41 @@ package main
 import (
 	"fmt"
 	"github.com/elsonwu/restapi"
-	"net/url"
+	// "net/url"
 )
 
 type User struct {
+	restapi.Api
 }
 
-func (self *User) GET(params restapi.Params) restapi.Output {
-	fmt.Println("User get ", params)
-	// output := restapi.Output{}
-	// output.Result = true
-	// output.Data = map[string]interface{}{"hello": "elson"}
-	// return output
+// func (self *User) GET(params restapi.Params) restapi.Output {
+// 	fmt.Println("User get ", params)
 
-	url := url.URL{}
-	query := url.Query()
-	query.Add("with_content_user", "1")
-	query.Add("with_content_comments", "1")
-	params2 := restapi.Params{Query: query}
-	return restapi.Call("content", "GET", params2)
-}
+// 	url := url.URL{}
+// 	query := url.Query()
+// 	query.Add("with_content_user", "1")
+// 	query.Add("with_content_comments", "1")
+// 	params2 := restapi.Params{Query: query}
+// 	return restapi.Call("content", restapi.MethodGet, params2)
+// }
 
 func (self *User) POST(params restapi.Params) restapi.Output {
 	fmt.Println("User post ", params)
-	return restapi.SetupOutput(true, restapi.Map{"hello": "xxx"})
+	return restapi.SetupOutput(true, restapi.Map{"hello": "xxx"}, nil)
 }
 
 type Content struct {
+	restapi.Api
 }
 
 func (self *Content) GET(params restapi.Params) restapi.Output {
 	fmt.Println("Content get ", params)
-	return restapi.SetupOutput(true, restapi.Map{"hello": "xxx"})
+	return restapi.SetupOutput(true, restapi.Map{"hello": "xxx"}, nil)
 }
 
 func (self *Content) POST(params restapi.Params) restapi.Output {
 	fmt.Println("Content post ", params)
-	return restapi.SetupOutput(true, restapi.Map{"hello": "xxx"})
+	return restapi.SetupOutput(true, restapi.Map{"hello": "xxx"}, nil)
 }
 
 func main() {
--- functions.go
+++ functions.go
@@ -2,11 +2,18 @@ package restapi
 
 import (
 	"encoding/json"
-	"fmt"
+	// "fmt"
 	"net/http"
 	"net/url"
 	"reflect"
 	"strings"
+)
+
+const (
+	MethodGet    string = "GET"
+	MethodPost   string = "POST"
+	MethodPut    string = "PUT"
+	MethodDelete string = "DELETE"
 )
 
 type Params struct {
@@ -15,19 +22,44 @@ type Params struct {
 
 type Map map[string]interface{}
 
-func SetupOutput(Result bool, Data Map) Output {
+func SetupOutput(Result bool, Data Map, errors []string) Output {
 	output := Output{}
 	output.Result = Result
 	output.Data = Data
+	output.Errors = errors
 	return output
 }
 
 type Output struct {
 	Result bool                   `json:"result"`
 	Data   map[string]interface{} `json:"data"`
+	Errors []string               `json:"errors"`
 }
 
-type IApi interface{}
+type IApi interface {
+	GET(Params) Output
+	POST(Params) Output
+	PUT(Params) Output
+	DELETE(Params) Output
+}
+
+type Api struct{}
+
+func (self *Api) GET(params Params) Output {
+	return SetupOutput(false, Map{}, []string{"Does not support get method"})
+}
+
+func (self *Api) POST(params Params) Output {
+	return SetupOutput(false, Map{}, []string{"Does not support post method"})
+}
+
+func (self *Api) PUT(params Params) Output {
+	return SetupOutput(false, Map{}, []string{"Does not support put method"})
+}
+
+func (self *Api) DELETE(params Params) Output {
+	return SetupOutput(false, Map{}, []string{"Does not support delete method"})
+}
 
 var apis map[string]IApi
 
@@ -50,11 +82,9 @@ func Call(apiName, method string, params Params) Output {
 		if v, ok := refReturn[0].Interface().(Output); ok {
 			return v
 		}
-	} else {
-		fmt.Println("api - " + apiName + " does not exist")
 	}
 
-	return Output{}
+	return SetupOutput(false, nil, []string{"api - " + apiName + " does not exist"})
 }
 
 func Run() {
